17d3f066487a0e1e192cdc277ec9efa34373904c
Gianluca-L <gluca1993.gl@gmail.com>
2019-11-18 17:46:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -149,13 +149,91 @@ var vera_amicizia_cits = ['L\'amicizia è la cosa più difficile al mondo da spi
 
 /// LASCIARE AMICI/SEPARAZIONE/ FERITI
 
+const sep_feriti_keywords = ['separazione', 'separarsi', 'separ', 'lasciare', 'ha lasciato', 'ha lasciato', 'lasciat', 'abbandonare', 'abbandon', 'finire', 'finita', 'finit', 'fin',
+   'perdere', 'perdit', 'perso', 'persa', 'perse','persi',
+   'dimenticare', 'dimenticat', 'dimentic', 'scordare', 'scordarsi', 'scord', 'mettere una pietra sopra', 'metterci una pietra sopra',
+   'ferit', 'ferire', 'feri', 'fare male', 'far male', 'fa male', 'farà male', 'offendere', 'offend', 'offes',
+   'tradire', 'tradiment', 'tradit', 'deludere', 'delud', 'delusion'];
+
+var sep_feriti_cits = ['A volte essere un amico significa padroneggiare l\'arte del tempismo. C\'è un momento per il silenzio. Un momento per lasciarsi andare e consentire alla gente di lanciarsi nel loro proprio destino. E un tempo per prepararsi a raccogliere i pezzi quando tutto è finito. (Octavia Estelle Butler)',
+    'Non piangere se un amico ti ha lasciato: solo quando ti avrà dimenticato potrai dire di averlo perso per sempre. (Jim Morrison)',
+    'Ci vuole un minuto per notare una persona speciale, un\'ora per apprezzarla, un giorno per volerle bene, tutta una vita per dimenticarla. (Charlie Chaplin)',
+    'La stretta di mano è per l’amicizia l’ultima lettera dell’alfabeto, per l’amore è la prima. (Rita Levi-Montalcini)',
+    'Non si ama qualcuno per tutta la vita; da questa speranza impossibile nascono adulterio, matricidio, tradimento dell\'amico. (Umberto Eco)',
+    'Non ti rimpiangerò mai o non dirò che avrei voluto non averti mai conosciuto, perché tanto tempo fa eri esattamente ciò di cui avevo bisogno. (Bob Marley)'];
+
+/// FALSA AMICIZIA/RELAZIONE NEGATIVA
+
+const falsa_amicizia_keywords = ['peggior nemico', 'peggior nemica', 'peggiore nemico', 'peggiore nemica', 'peggiori nemici', 'peggiori nemiche','nemic',
+     'fals', 'menzogn', 'illusione', 'illusor', 'illus', 'sbaglia', 'fint', 'scorrett', 'fasull', 'ipocri', 'bugiard', 'sleal', 'doppiogiochista', 'doppiogioco', 'doppi', 'ingann', 'negat'];
+
+var falsa_amicizia_cits = ['Alla fine, ci ricorderemo non le parole dei nostri nemici, ma il silenzio dei nostri amici. (Martin Luther King)',
+      'La peggiore solitudine è essere privi di sincera amicizia. (Francis Bacon)',
+      'È difficile sapere cosa sia la verità, ma a volte è molto facile riconoscere una falsità. (Albert Einstein)',
+      'È più vergognoso non fidarsi dei propri amici che esserne ingannati. (François de la Rochefoucauld)',
+      'Troppo spesso togliamo tempo ai nostri amici per dedicarlo ai nostri nemici. (Hermann Hesse)',
+      'Il tuo peggior nemico potrebbe essere il tuo migliore amico e il tuo migliore amico potrebbe essere il tuo peggior nemico. (Bob Marley)',
+      'Un amico è solo un nemico che conosci. (Kurt Cobain)',
+      'O l’amicizia preziosa o l’ostilità durissima. (Benito Mussolini)',
+      'Alcune persone non meritano il nostro sorriso, figuriamoci le nostre lacrime. (Charles Bukowski)',
+      'Non siamo mai così indifesi verso la sofferenza, come nel momento in cui amiamo. (Sigmund Freud)'];
+
+
+////////// AMORE
+
+const amore_Cat_keywords = ['amore', 'amor', 'amare', 'amo', 'ami','ama','amiamo','amate','amano','innamorarsi', 'innamor',
+      'mi piace', 'ti piace', 'gli piace', 'piacciono', 'gli piaccio', 'le piaccio', 'piace', 'compagno', 'compagna', 'compagne', 'compagni',
+      'fidanz', 'marit', 'mogli', 'moros', 'mio tipo', 'mia tipa', 'tip'];
+
+
+/// LASCIARE/ABBANDONO/ESSERE FERITI
+
+const lasciare_keywords = ['lasciare', 'lasciat', 'abband', 'finire', 'fine', 'finit', 'separ', 'perd', 'perso', 'persa', 'perse', 'persi', 'romp', 'rott', 'moll', 'smett', 'smess',
+       'dimenticare', 'dimentic', 'scordare', 'scordarsi', 'scord', 'mettere una pietra sopra', 'metterci una pietra sopra', 'scomp',
+       'ferit', 'ferir', 'fare male', 'far male', 'fa male', 'male', 'offend', 'offes', 'delus', 'soffr', 'soffer', 'dolor'];
+
+var lasciare_cits = ['I venti che a volte portano via qualcosa che amiamo sono gli stessi che ci portano qualcosa da imparare ad amare. Quindi non dovremmo piangere per qualcosa che ci è stato portato via ma, sì, amare ciò che ci è stato offerto. Perché ciò che è veramente nostro ci appartiene per sempre. (Bob Marley)',
+       'Non ci può essere profonda delusione dove non c\'è un amore profondo. (Martin Luther King)',
+       'Tu e io non siamo che una cosa sola. Non posso farti del male senza ferirmi. (Mahatma Gandhi)',
+       'L’amore è una promessa, l’amore è un ricordo, una volta donato non può essere dimenticato, non può mai scomparire. (John Lennon)',
+       'Quando un amore finisce, uno dei due soffre. Se non soffre nessuno, non è mai iniziato. Se soffrono entrambi, non è mai finito. (Marilyn Monroe)',
+       'Non si ama qualcuno per tutta la vita; da questa speranza impossibile nascono adulterio, matricidio, tradimento dell\'amico. (Umberto Eco)',
+       'Smettere di amare è come perdere peso. è molto più semplice metterne su che perderlo. (Aretha Franklin)',
+       'Non ti rimpiangerò mai o non dirò che avrei voluto non averti mai conosciuto, perché tanto tempo fa eri esattamente ciò di cui avevo bisogno. (Bob Marley)',
+       'Alcune persone non meritano il nostro sorriso, figuriamoci le nostre lacrime. (Charles Bukowski)',
+     ]
+
+/// INNAMORARSI/AMORE VERO
+
+const amore_vero_keywords = ['vero', 'veramente','ver', 'genuin', 'sincer', 'autenticamente', 'autent', 'schiett', 'dissimul', 'miglior', 'puro', 'pura', 'pure', 'puri', 'purezza', 'saldo', 'salda', 'salde', 'saldi', 'degn', 'solid', 'spontan', 'incondiz',
+      'significat', 'senso', 'sensi', 'valor', 'import',
+      'capir', 'capito', 'capita', 'capite', 'capiti', 'comprend', 'nasc',
+      'fid', 'affid', 'rispett',
+      'complet', 'passione', 'emoz', 'slancio', 'ard', 'trasporto'];
+
+var amore_vero_cits = ['L\'amore è reale, la realtà è amore | l\'amore è sentire, sentire l\'amore | l\'amore è voler essere amati. (John Lennon)',
+      'L\'amore vero vuole il bene dell\'amato. (Umberto Eco)',
+      'Se ti attendi qualche forma di ricompensa, non è amore: l\'amore vero è amare senza condizioni e senza aspettative. (Madre Teresa di Calcutta)',
+      'L\'amore è una forma di pregiudizio. Si ama quello di cui si ha bisogno, quello che ci fa star bene, quello che ci fa comodo. Come fai a dire che ami una persona, quando al mondo ci sono migliaia di persone che potresti amare di più, se solo le incontrassi? Il fatto è che non le incontri. (Charles Bukowski)',
+      '“Bimbo mi chiedi cos\'è l\'amore? Cresci e lo saprai. Bimbo mi chiedi cos\'è la felicità? Rimani bimbo e lo vedrai... (Jim Morrison)',
+      'Io non pretendo di sapere cosa sia l’amore per tutti, ma posso dirvi che cosa è per me: l’amore è sapere tutto su qualcuno, è avere la voglia di essere ancora con lui più che con ogni altra persona. L’amore è la fiducia di dirgli tutto su voi stessi, compreso le cose che ci potrebbero far vergognare. L’amore è sentirsi a proprio agio e al sicuro... (Albert Einstein)',
+      'L\'amore non vive di parole, né può essere spiegato a parole. (Madre Teresa di Calcutta)',
+      'L\'Amore non è una passione. L\'Amore non è una emozione. L\'amore è una comprensione profonda del fatto che in qualche modo l\'altro ti completa. Qualcuno ti rende un cerchio perfetto; la presenza dell\'altro rinforza la tua presenza. (Osho)',
+      'Trova qualcuno che ti faccia dimenticare il tuo passato, la tristezza. Trova qualcuno che ti cambi la vita, che la renda migliore, che sostituisca e riempia il vuoto di chi se n\'è andato. Trova qualcuno per cui valga la pena sorridere. (Marilyn Monroe)',
+      'Ho quella disperata sensazione che niente abbia senso. Allora decido di innamorarmi, ma è troppo difficile. Voglio dire, pensi costantemente a una persona ed è soltanto fantasia, non è reale, e poi diventa una cosa così coinvolgente, devi vederla di continuo e va a finire che è un lavoro come un altro. (Andy Warhol)',
+      'Liberate l\'amore oppure liberatevene per sempre. (Jim Morrison)',
+      'L\'amore non chiede mai, dà sempre. (Mahatma Gandhi)',
+      'Colui che l\'amore tocca non cammina nell’oscurità. (Platone)',
+      'Non confondere l\'amore col delirio del possesso, che causa le sofferenze più atroci. Perché contrariamente a quanto comunemente si pensa, l\'amore non fa soffrire. Quello che fa soffrire è l\'istinto della proprietà, che è il contrario dell\'amore. (Antoine de Saint-Exupéry)'];
+
+/// INNAMORARSI/AMORE VERO
 
 function getInputValue() {
   // Selecting the input element and get its value
   var textFromInput = document.getElementById("myInput").value;
   var text_From_Voice = document.getElementById("myVoiceInput").innerHTML;
-  var inputVal = text_From_Voice.toLowerCase();
-  //var inputVal = textFromInput.toLowerCase();
+  //var inputVal = text_From_Voice.toLowerCase();
+  var inputVal = textFromInput.toLowerCase();
   console.log(inputVal);
   var check = document.getElementById("writeCheckbox").value;
   console.log(check);
@@ -211,13 +289,38 @@ function getInputValue() {
     alert(se_stessi_cit);
   }
   if (amicizia_Cat_keywords.some(keyword => inputVal.includes(keyword))) {
-    if (vera_amicizia_keywords.some(keyword => inputVal.includes(keyword))) {
+
+  }
+  if (vera_amicizia_keywords.some(keyword => inputVal.includes(keyword))) {
+    console.log("Found")
+    // audio.src = a_music;
+    // audio.play();
+    var vera_amicizia_cit = vera_amicizia_cits[Math.floor(Math.random() * vera_amicizia_cits.length)];
+    alert(vera_amicizia_cit);
+  }
+  if (sep_feriti_keywords.some(keyword => inputVal.includes(keyword))) {
+    console.log("Found")
+    // audio.src = a_music;
+    // audio.play();
+    var sep_feriti_cit = sep_feriti_cits[Math.floor(Math.random() * sep_feriti_cits.length)];
+    alert(sep_feriti_cit);
+  }
+  if (falsa_amicizia_keywords.some(keyword => inputVal.includes(keyword))) {
+    console.log("Found")
+    // audio.src = a_music;
+    // audio.play();
+    var falsa_amicizia_cit = falsa_amicizia_cits[Math.floor(Math.random() * falsa_amicizia_cits.length)];
+    alert(falsa_amicizia_cit);
+  }
+  if (amore_Cat_keywords.some(keyword => inputVal.includes(keyword))) {
+    if (amore_vero_keywords.some(keyword => inputVal.includes(keyword))) {
       console.log("Found")
       // audio.src = a_music;
       // audio.play();
-      var vera_amicizia_cit = vera_amicizia_cits[Math.floor(Math.random() * vera_amicizia_cits.length)];
-      alert(vera_amicizia_cit);
+      var amore_vero_cit = amore_vero_cits[Math.floor(Math.random() * amore_vero_cits.length)];
+      alert(amore_vero_cit);
     }
   }
+
 
 }
